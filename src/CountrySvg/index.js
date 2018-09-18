6226bf63ea15a50bc08911a76d6ab1a67ecc610b
Paco Gómez Granados <pacogomezgranados@gmail.com>
2018-09-18 17:23:53 +0200

    Created svgs are squared now

--- src/CountrySvg/index.js
+++ src/CountrySvg/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import geojson2svg from 'geojson-to-svg';
-import { getBoundaries } from '../utils/geojsonUtils';
+// import { getBoundaries } from '../utils/geojsonUtils';
 import merc from 'mercator-projection';
 
 import './style.css';
@@ -13,27 +13,14 @@ class CountrySvg extends Component {
 
         this.state = {
             loading: true,
-            svg: null,
+            svg: null
         };
         this.svgRef = React.createRef();
+        this.initSvg = this.initSvg.bind(this);
     }
 
     componentDidMount() {
-        const svg = geojson2svg()
-            .styles({ 'MultiPolygon' : { fill: 'black', stroke: 'none' } })
-            .projection((coord) => {
-                const projected = merc.fromLatLngToPoint({lat: coord[1], lng: coord[0]});
-                return [projected.x, projected.y];
-            })
-            .data(this.props.countryInfo)
-            .render();
-
-        this.setState({
-            loading: false,
-            svg: {
-                __html: svg
-            },
-        });
+        setTimeout(this.initSvg);
     }
 
     render() {
@@ -66,6 +53,17 @@ class CountrySvg extends Component {
         );
     }
 
+    initSvg() {
+        const svgElement = this.getSvgElement();
+        makeSvgElementSquare(svgElement);
+        this.setState({
+            loading: false,
+            svg: {
+                __html: svgElement.outerHTML
+            }
+        });
+    }
+
     canShowDot() {
         const result = this.props.latLonToProject && this.props.countryInfo && this.svgRef.current;
         return result;
@@ -74,7 +72,7 @@ class CountrySvg extends Component {
     getDotStyle() {
         const pixelInfo = project(
             this.props.latLonToProject,
-            getBoundaries(this.props.countryInfo),
+            getSvgBoundaries(this.svgRef.current.querySelector('svg')),
             this.svgRef.current.getBoundingClientRect(),
         );
         return {
@@ -82,25 +80,68 @@ class CountrySvg extends Component {
             left: `${pixelInfo.x}px`,
         };
     }
+
+    getSvgElement() {
+        const svg = geojson2svg()
+            .styles({ 'MultiPolygon' : { fill: 'black', stroke: 'none' } })
+            .projection((coord) => {
+                const projected = merc.fromLatLngToPoint({lat: coord[1], lng: coord[0]});
+                return [projected.x, projected.y];
+            })
+            .data(this.props.countryInfo)
+            .render();
+
+        const parser = new DOMParser();
+        return parser.parseFromString(svg, "image/svg+xml").querySelector('svg');
+    }
 }
 
-function project(latlon, latlonBoundaries, boundingBox) {
-    const minLat = latlonBoundaries.bottomRight[0];
-    const maxLat = latlonBoundaries.topLeft[0];
-    const minLon = latlonBoundaries.topLeft[1];
-    const maxLon = latlonBoundaries.bottomRight[1];
-
-    const relativeX = getRelativePosition(latlon.lon, minLon, maxLon);
-    const relativeY = getRelativePosition(latlon.lat, minLat, maxLat);
+function project(latlon, svgBoundaries, boundingBox) {
+    const { minX, minY, maxX, maxY } = svgBoundaries;
+    const {x, y} = merc.fromLatLngToPoint({lat: latlon.lat, lng: latlon.lon});
+    const relativeX = getRelativePosition(x, minX, maxX);
+    const relativeY = getRelativePosition(y, minY, maxY);
     return {
         x: relativeX * boundingBox.width,
-        y: (1 - relativeY) * boundingBox.height,
+        y: relativeY * boundingBox.height,
     };
 
 }
 
+function getSvgBoundaries(element) {
+    const viewBox = element.getAttribute('viewBox');
+    const [minX, minY, width, height] = viewBox.split(' ').map(string => parseFloat(string));
+
+    return {
+        minX,
+        minY,
+        maxX: minX + width,
+        maxY: minY + height
+    };
+}
+
 function getRelativePosition(x, min, max) {
     return (x - min) / (max - min);
+}
+
+function makeSvgElementSquare(element) {
+
+    const viewBox = element.getAttribute('viewBox');
+    let [minX, minY, width, height] = viewBox.split(' ').map(string => parseFloat(string));
+
+    if(width < height) {
+        const diff = height - width;
+        minX = minX - (diff / 2);
+        width = height;
+    } else {
+        const diff = width - height;
+        minY = minY - (diff / 2);
+        height = width;
+    }
+
+    element.setAttribute('viewBox', `${minX} ${minY} ${width} ${height}`);
+
+    return element;
 }
 
 CountrySvg.propTypes = {
