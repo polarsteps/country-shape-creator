9658be9cd235c7d49bbca2776335ee52b44a9855
Paco Gómez Granados <pacogomezgranados@gmail.com>
2018-10-23 10:34:47 +0200

    Updates

---
+++ precache-manifest.1f71d18c90da51f9dc4acda780d0ed92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "89072e38b496520decb1",
+    "url": "./static/js/main.89072e38.chunk.js"
+  },
+  {
+    "revision": "e2e0e5477107c27f09bb",
+    "url": "./static/js/1.e2e0e547.chunk.js"
+  },
+  {
+    "revision": "89072e38b496520decb1",
+    "url": "./static/css/main.80adee52.chunk.css"
+  },
+  {
+    "revision": "ddf68d133a82f03eabe3aede0d665d87",
+    "url": "./index.html"
+  }
+];
